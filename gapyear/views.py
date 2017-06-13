590de423a7d323c9480318d08434ee4f01a6b42a
Sakshi <sakshi@Sakshis-MacBook-Air.local>
2017-06-13 14:09:55 +0530

    added one more view

--- gapyear/views.py
+++ gapyear/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
     return HttpResponse("This is the home page")
+
+#making some changes here
+
+def next_page(request):
+    return HttpResponse("This will be the next page")
